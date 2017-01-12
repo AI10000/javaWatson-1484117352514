82e96ebc17dc21d7813d4e853807fa9d27801bf7
凌太 稲葉 <inaba.ryota@tis.co.jp>
2017-01-11 22:57:03 -0600

    import

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -7,10 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.ibm.watson.developer_cloud.natural_language_classifier.*;
-/*import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
+import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classifier;
-*/
+
 
 /**
  * Servlet implementation class SimpleServlet
